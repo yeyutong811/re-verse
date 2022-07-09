ec09e2e55f36cf6ff1fde98f949cce40308246aa
angjiahui <angjiahui@users.noreply.github.com>
2022-07-10 00:59:35 +0800

    Added some buttons

--- re-verse_frontend/src/Home.js
+++ re-verse_frontend/src/Home.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Layout } from "antd";
 
+import { Button, Card, Grid } from "@material-ui/core";
+
+import "./App.css";
+// import CustomBeatLoader from "./components/CustomBeatLoader";
+
 const { Content } = Layout;
 
 const Home = () => {
@@ -17,6 +22,17 @@ const Home = () => {
       >
         <h2>home</h2>
         {/* <h3>Internet-of-Things Enabled Hospital Wearables</h3> */}
+        <Grid item xs={12}>
+          <div className="upload-btn">
+            {/* {submitting ? (
+                    <CustomBeatLoader submitting={submitting} />
+                  ) : ( */}
+            {/* <form onSubmit={handleSubmit(submit)} className="uploads"> */}
+
+            {/* </form> */}
+            {/* )} */}
+          </div>
+        </Grid>
       </Content>
     </Layout>
   );
--- re-verse_frontend/src/Start.js
+++ re-verse_frontend/src/Start.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Layout } from "antd";
+import { Button, Card, Grid } from "@material-ui/core";
+
 
 const { Content } = Layout;
 
@@ -15,9 +17,28 @@ const AboutUs = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Us</h2>
-        {/* <h3>Internet-of-Things Enabled Hospital Wearables</h3> */}
-      </Content>
+        <h2>Get Started!</h2>
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+        >
+          10 recoins
+        </Button>
+        <h2>wallet</h2>
+        <h3>Use your re-coins for vouchers or support environmental projects</h3>
+        <h2>scan</h2>
+        <h3>scan the item and check if it is recycable and earn re-coins!</h3>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+        >
+          Logout
+        </Button>      </Content>
     </Layout>
   );
 };
